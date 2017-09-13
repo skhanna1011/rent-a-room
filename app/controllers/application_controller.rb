8ba6acf294f76c34d3e18c03ee36c304babf71f5
skhanna1011 <skhanna1011@gmail.com>
2017-09-13 17:12:21 +0530

    Fixes sign_in with usernam

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :mobile_no, :first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :mobile_no, :first_name, :last_name])
+    
   end
 end
