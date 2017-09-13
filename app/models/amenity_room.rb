278785f6348626851ff7905245b4d5d453307d9c
skhanna1011 <skhanna1011@gmail.com>
2017-09-13 17:39:21 +0530

    Adds Associations

--- app/models/amenity_room.rb
+++ app/models/amenity_room.rb
@@ -1,2 +1,4 @@
 class AmenityRoom < ApplicationRecord
+  belongs_to :room
+  belongs_to :amenity
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  belongs_to :user
+  belongs_to :city
+  has_many :bookings
+  has_many :amenity_rooms
+  has_many :amenities, through: :amenity_rooms
 end
