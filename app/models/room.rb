fde61c1d5ecc11224922c89248f2ce126427bbf2
skhanna1011 <skhanna1011@gmail.com>
2017-09-14 20:41:31 +0530

    Adds Validations and functionality to get latitude and longituder geocoding api

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,7 +5,20 @@ class Room < ApplicationRecord
   has_many :amenity_rooms
   has_many :amenities, through: :amenity_rooms
 
-  validates :name, :address, :city, :user, presence: true
+  before_validation :set_latitude_longitude
+
+  validates :name, :address, :user, presence: true
   validates :name, :address, uniqueness: true
   validates :price, :latitude, :longitude, numericality: true
+
+  private
+
+  def set_latitude_longitude
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{self.address},&key=Api_key")
+    result = JSON.parse(response.body)
+    if !result.has_key?("error_message")
+      self.latitude = result["results"][0]["geometry"]["location"]["lat"]
+      self.longitude = result["results"][0]["geometry"]["location"]["lng"]
+    end
+  end
 end
